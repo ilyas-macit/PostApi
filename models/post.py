340f2e1b16ab34dbc4b5ff3965eaf8abd0bd4483
ilyas-macit <59144340+ilyas-macit@users.noreply.github.com>
2023-09-13 00:05:06 +0300

    post base model updated

--- models/post.py
+++ models/post.py
@@ -4,13 +4,12 @@ from datetime import datetime
 
 
 class Post(BaseModel):
-    id: str
-    title: str
-    short_description: str
-    description: str
-    tags: List[str]
-    created_at: datetime
-    updated_at: datetime
+    title: str = None
+    short_description: str = None
+    description: str = None
+    tags: List[str] = None
+    created_at: datetime = None
+    updated_at: datetime = None
 
 
 class PostUpdateDto(BaseModel):
