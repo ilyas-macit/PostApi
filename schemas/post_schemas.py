29427003a53d1282e38b8299c5f9452ded201a14
ilyas-macit <59144340+ilyas-macit@users.noreply.github.com>
2023-09-12 16:25:14 +0300

    Post get schemas added

--- schemas/post_schemas.py
+++ schemas/post_schemas.py
@@ -24,3 +24,17 @@ def post_update_entity(item: PostUpdateDto) -> dict:
         entity['tags'] = item.tags
     entity['updated_at'] = datetime.now()
     return entity
+
+def post_get_entity(item) -> dict:
+    return {
+        'title': item['title'],
+        'short_description': item['short_description'],
+        'description': item['description'],
+        'tags': item['tags'],
+        'created_at': datetime.now(),
+        'updated_at': None
+    }
+
+
+def post_get_all_entities(items) -> list:
+    return [post_get_entity(item) for item in items]
