a4c200d170adad2e1548f0d463476eb20c1c14b3
ilyas-macit <59144340+ilyas-macit@users.noreply.github.com>
2023-09-13 00:07:42 +0300

    post_router Exception messages updated

--- routers/post_router.py
+++ routers/post_router.py
@@ -10,29 +10,30 @@ def get_all_post():
     try:
         return get_all()
     except Exception as e:
-        return HTTPException(status_code= 404, detail= e)
+        return HTTPException(status_code= 400, detail= str(e))
 
 @posts.get("/posts/{id}")
 def get_post_by_id(id):
     try:
         return get_by_id(id)
     except Exception as e:
-        return HTTPException(status_code= 404, detail= e)
+        return HTTPException(status_code= 400, detail= str(e))
 
 @posts.post("/posts/")
-def create(post: Post):
+def create(post : Post):
     try:
         create_post(post)
-
     except Exception as e:
-        return HTTPException(status_code= 404, detail= e)
+        return HTTPException(status_code= 400, detail= str(e))
+
+
 
 @posts.put("/posts/{id}")
 def update(id, post: PostUpdateDto):
     try:
         update_post(id,post)
     except Exception as e:
-        return HTTPException(status_code= 404, detail= e)
+        return HTTPException(status_code= 400, detail= str(e))
 
 
 @posts.delete("/posts/{id}")
@@ -40,4 +41,4 @@ def delete(id):
     try:
         delete_post(id)
     except Exception as e:
-        return HTTPException(status_code=404, detail=e)
+        return HTTPException(status_code=400, detail=str(e))
