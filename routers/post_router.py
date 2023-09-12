92b78fde16d12ead1eabc612d781e7bca847426d
ilyas-macit <59144340+ilyas-macit@users.noreply.github.com>
2023-09-12 18:02:02 +0300

    post_router created

---
+++ routers/post_router.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, HTTPException
+from models.post import Post
+from services.post_service import *
+
+posts = APIRouter()
+
+
+@posts.get("/posts/")
+def get_all_post():
+    try:
+        return get_all()
+    except Exception as e:
+        return HTTPException(status_code= 404, detail= e)
+
+@posts.get("/posts/{id}")
+def get_post_by_id(id):
+    try:
+        return get_by_id(id)
+    except Exception as e:
+        return HTTPException(status_code= 404, detail= e)
+
+@posts.post("/posts/")
+def create(post: Post):
+    try:
+        create_post(post)
+
+    except Exception as e:
+        return HTTPException(status_code= 404, detail= e)
+
+@posts.put("/posts/{id}")
+def update(id, post: PostUpdateDto):
+    try:
+        update_post(id,post)
+    except Exception as e:
+        return HTTPException(status_code= 404, detail= e)
+
+
+@posts.delete("/posts/{id}")
+def delete(id):
+    try:
+        delete_post(id)
+    except Exception as e:
+        return HTTPException(status_code=404, detail=e)
