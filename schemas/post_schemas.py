e0982af602033dfe6e93cc84a93377a36a91c3fa
ilyas-macit <59144340+ilyas-macit@users.noreply.github.com>
2023-09-12 14:57:19 +0300

    Post Schemas created

---
+++ schemas/post_schemas.py
@@ -0,0 +1,26 @@
+from models.post import Post, PostUpdateDto
+from datetime import datetime
+
+def post_create_entity(item: Post) -> dict:
+    return {
+        'title': item.title,
+        'short_description': item.short_description,
+        'description': item.description,
+        'tags': item.tags,
+        'created_at': datetime.now(),
+        'updated_at': None
+    }
+
+
+def post_update_entity(item: PostUpdateDto) -> dict:
+    entity = {}
+    if hasattr(item, "title"):
+        entity['title'] = item.title
+    if hasattr(item, "short_description"):
+        entity['short_description'] = item.short_description
+    if hasattr(item, "description"):
+        entity['description'] = item.description
+    if hasattr(item, "tags"):
+        entity['tags'] = item.tags
+    entity['updated_at'] = datetime.now()
+    return entity
